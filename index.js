d056fcb653002d1b20ac1d73d3a5000c1018808e
Adrienne Miranda <adrienne.c.miranda@gmail.com>
2019-05-28 20:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPLay (musicians, intruments){
   let array = []
   for (let i = 0; i<musicians.length; i++){
-    let string = `$musicians[i] plays $instruments[i]`
+    let string = `${musicians[i]} plays ${instruments[i]}`
     array.push(string)
   }
 }
