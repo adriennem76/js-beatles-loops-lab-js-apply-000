97c8676547e5674a7e30c24f1bc64161d712d7a5
Adrienne Miranda <adrienne.c.miranda@gmail.com>
2019-05-28 20:14:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPLay (musicians, intruments){
+  let array = []
+  for (let i = 0; i<musicians.length; i++){
+    let string = `$musicians[i] plays $instruments[i]`
+    array.push(string)
+  }
+}
